3abc49870aa64f32b4e3f7b5c5db75cccb186e72
korokarashi <koro.karashi@gmail.com>
2018-07-06 15:07:24 +0700

    Nguyen Thanh

--- quocthang/js/jquery.js
+++ quocthang/js/jquery.js
@@ -3,15 +3,3 @@ $(document).ready(function() {
 		interval: 5000
 	})
 });
-
-window.onscroll = function() {myFunction()};
-var header = document.getElementById("headMain");
-var h = document.getElementById("header");
-var sticky = h.offsetHeight;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
