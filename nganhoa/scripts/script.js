fc8863da8b8f274a7840bfc557f29e86d0c28780
nguyenthanh1606 <nguyenthanh.bt1606@gmail.com>
2018-09-26 10:04:46 +0700

    create layout web

---
+++ nganhoa/scripts/script.js
@@ -0,0 +1,36 @@
+function pushMenuOpen(){
+    let b = document.getElementById('menu-mobile');
+    b.style.right = '0';
+    let c = document.getElementById('hide-w');
+    c.style.display = 'block';
+}
+function pushMenuClose(){
+    let b = document.getElementById('menu-mobile');
+    b.style.right = '-100%';
+    let c = document.getElementById('hide-w');
+    c.style.display = 'none';
+}
+
+$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    // $(window).scroll(function(){
+    //     if ($(this).scrollTop() > 200) {
+    //         $('.scrollToTop').fadeIn();
+    //     } else {
+    //         $('.scrollToTop').fadeOut();
+    //     }
+    // });
+
+    //Click event to scroll to top
+    // $('.scrollToTop').click(function(){
+    //     $('html, body').animate({scrollTop : 0},500);
+    //     return false;
+    // });
+
+    $('#hide-w').click(function(){
+        $('#menu-mobile').css('right','-100%');
+        $('#hide-w').css('display',('none'));
+    });
+
+});
