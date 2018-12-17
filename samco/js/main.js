08a8470811278064261a91e95878c5e481666388
nguyenthanh1606 <nguyenthanh.bt1606@gmail.com>
2018-12-17 09:59:23 +0700

    save

--- samco/js/main.js
+++ samco/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         responsive: true,
         responsive: {
             0: {
-                items: 2
+                items: 3
             },
             600: {
                 items: 4
@@ -19,4 +19,41 @@ $(document).ready(function () {
     $(document).ready(function () {
         $('.single-item').slick();
     });;
+});
+
+function pushMenuOpen(){
+    let b = document.getElementById('menu-mobile');
+    b.style.right = '0';
+    let c = document.getElementById('hide-w');
+    c.style.display = 'block';
+}
+function pushMenuClose(){
+    let b = document.getElementById('menu-mobile');
+    b.style.right = '-100%';
+    let c = document.getElementById('hide-w');
+    c.style.display = 'none';
+}
+
+$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    // $(window).scroll(function(){
+    //     if ($(this).scrollTop() > 200) {
+    //         $('.scrollToTop').fadeIn();
+    //     } else {
+    //         $('.scrollToTop').fadeOut();
+    //     }
+    // });
+
+    //Click event to scroll to top
+    // $('.scrollToTop').click(function(){
+    //     $('html, body').animate({scrollTop : 0},500);
+    //     return false;
+    // });
+
+    $('#hide-w').click(function(){
+        $('#menu-mobile').css('right','-100%');
+        $('#hide-w').css('display',('none'));
+    });
+
 });
