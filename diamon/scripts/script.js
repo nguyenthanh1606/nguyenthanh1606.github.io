b7ec5d983d13d1c3be4e1916033991b268393b11
nguyenthanh1606 <nguyenthanh.bt1606@gmail.com>
2019-04-23 17:20:18 +0700

    save

--- diamon/scripts/script.js
+++ diamon/scripts/script.js
@@ -1,27 +1,31 @@
-var swiper = new Swiper('.slider-partner', {
-    slidesPerView: 5,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1024: {
-            slidesPerView: 4,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        640: {
-            slidesPerView: 2,
-        },
-        320: {
-            slidesPerView: 2,
-        }
-    }
+$(document).ready(function () {
+  $('.list-htContact .collapse').collapse({toggle: false});
 });
+
+var swiper = new Swiper('.slider-partner', {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4
+    },
+    768: {
+      slidesPerView: 3
+    },
+    640: {
+      slidesPerView: 2
+    },
+    320: {
+      slidesPerView: 2
+    }
+  }
+})
